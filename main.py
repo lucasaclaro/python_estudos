520cddd911024f0160f4c2d9b109bc0ef38afe74
Lucas Claro <lucas-claro@hotmail.com>
2023-06-09 09:44:51 -0300

    comandos match e comandos de iteração

---
+++ main.py
@@ -0,0 +1,34 @@
+# Instrução match
+
+def http_error(status):
+    match status:
+        case 400:
+            return 'Bad request'
+        case 404:
+            return 'Not found'
+        case 418:
+            return 'I am a teapot'
+        case 401 | 403: # uma condição ou outra
+            return 'Not allowed'
+        case _: # operador curinga
+            return 'Something is wrong with the internet'
+
+print(http_error(401))
+
+
+# Criar lista com range
+
+nova_lista = list(range(5, 10))
+print(nova_lista)
+
+
+
+# Iterar sobre os índices de uma sequência
+
+lista_nomes = ['Spider-Man', 'Iron-Man', "American Captain"]
+
+for i in range(len(lista_nomes)):
+    print(f'{i}: {lista_nomes[i]}')
+
+# ver técnicas de iteração: https://docs.python.org/pt-br/3/tutorial/datastructures.html#tut-loopidioms
+# continuar daqui: https://docs.python.org/pt-br/3/tutorial/controlflow.html#pass-statements
